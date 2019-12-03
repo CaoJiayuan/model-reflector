07d28310f11771e36240f68585068ecbdff36dde
caojiayuan <cjy632258@hotmail.com>
2019-12-03 17:53:01 +0800

    update validate

--- src/ModelReflector.php
+++ src/ModelReflector.php
@@ -46,37 +46,53 @@ class ModelReflector
             if (method_exists($this, $setMethod)) {
                 $this->$setMethod($value);
             } else if (property_exists($this, $key)) {
-                /** @var Type[] $types */
-                $types = $this->propertyInfo->getTypes(static::class, $key);
-                if ($types) {
-                    foreach ($types as $type) {
-                        $builtinType = $type->getBuiltinType();
-                        $class = $type->getClassName();
-                        $collection = $type->isCollection();
-                        if ($builtinType == 'object' && is_subclass_of($class,ModelReflector::class)) {
-                            $this->$key = $class::make($value);
-                        } elseif ($this->isCommonType($builtinType)) {
-                            $this->$key = $this->castCommonType($builtinType, $value);
-                        } elseif ($collection) {
-                            $ct = $type->getCollectionValueType();
-                            $itemClass = $ct->getClassName();
-                            if (!is_subclass_of($itemClass,ModelReflector::class)) {
-                                throw new \InvalidArgumentException("Model property [{$key}] must an array of instance of ModelReflector");
-                            }
-                            foreach ($value as $k => $v) {
-                                $this->$key[$k] = $itemClass::make($v);
-                            }
-                        } else {
-                            $this->$key = $value;
-                        }
-
-                        break;
-                    }
+                if (is_null($value)) {
+                    $this->$key = null;
                 } else {
-                    $this->$key = $value;
+                    /** @var Type[] $types */
+                    $types = $this->propertyInfo->getTypes(static::class, $key);
+                    if ($types) {
+                        foreach ($types as $type) {
+                            $builtinType = $type->getBuiltinType();
+                            $class = $type->getClassName();
+                            $collection = $type->isCollection();
+                            if ($builtinType == 'object' && is_subclass_of($class,ModelReflector::class)) {
+                                $this->validateReflectData($value, $class);
+                                $this->$key = $class::make($value);
+                            } elseif ($this->isCommonType($builtinType)) {
+                                $this->$key = $this->castCommonType($builtinType, $value);
+                            } elseif ($collection) {
+                                $ct = $type->getCollectionValueType();
+                                $itemClass = $ct->getClassName();
+                                if (!is_subclass_of($itemClass,ModelReflector::class)) {
+                                    throw new \InvalidArgumentException("Model property [{$key}] must an array of instance of ModelReflector");
+                                }
+                                foreach ($value as $k => $v) {
+                                    $this->validateReflectData($value, $class);
+                                    $this->$key[$k] = $itemClass::make($v);
+                                }
+                            } else {
+                                $this->$key = $value;
+                            }
+
+                            break;
+                        }
+                    } else {
+                        $this->$key = $value;
+                    }
                 }
             }
         }
+    }
+
+    protected function validateReflectData($value, $class)
+    {
+        if (!is_array($value)) {
+            $type = gettype($value);
+            throw new \InvalidArgumentException("Try to reflect [$type] to [$class]");
+        }
+
+        return true;
     }
 
     protected function castCommonType($type, $value)
