d4f42f0c704eb6b73dde4ab3151dce301be425eb
caojiayuan <cjy632258@hotmail.com>
2019-12-05 17:15:30 +0800

    cache reflect model

--- src/Reflector.php
+++ src/Reflector.php
@@ -14,58 +14,41 @@ class Reflector
 
     protected static $cacheDir;
 
-    public static function loadFromArray($clz, $data)
+    protected static $cacheClasses = [];
+
+
+    /**
+     * [
+     *      'App\\' => '/home/www/project/app'
+     * ]
+     *
+     * @param $psr4Dirs
+     * @param \Closure|null $afterEach
+     */
+    public static function makeCaches($psr4Dirs, \Closure $afterEach = null)
     {
-        if (is_object($clz)) {
-            $instance = $clz;
-        } else {
-            $instance = new $clz;
-        }
-
-        foreach ($data as $key => $value) {
-            $setMethod = 'set' . ucfirst($key);
-            if (method_exists($instance, $setMethod)) {
-                $instance->$setMethod($value);
-            } else if (property_exists($instance, $key)) {
-                if (is_null($value)) {
-                    $instance->$key = null;
-                } else {
-                    /** @var Type[] $types */
-                    $types = self::getPropertyInfo()->getTypes($clz, $key);
-                    if ($types) {
-                        foreach ($types as $type) {
-                            $builtinType = $type->getBuiltinType();
-                            $class = $type->getClassName();
-                            $collection = $type->isCollection();
-                            if ($builtinType == 'object' && is_subclass_of($class, ModelReflector::class)) {
-                                self::validateReflectData($value, $class);
-                                $instance->$key = self::loadFromArray($class, $value);
-                            } elseif (self::isCommonType($builtinType)) {
-                                $instance->$key = self::castCommonType($builtinType, $value);
-                            } elseif ($collection) {
-                                $ct = $type->getCollectionValueType();
-                                $itemClass = $ct->getClassName();
-                                if (!is_subclass_of($itemClass, ModelReflector::class)) {
-                                    throw new \InvalidArgumentException("Model property [{$key}] must an array of instance of ModelReflector");
-                                }
-                                foreach ($value as $k => $v) {
-                                    self::validateReflectData($value, $itemClass);
-                                    $instance->$key[$k] = self::loadFromArray($itemClass, $v);
-                                }
-                            } else {
-                                $instance->$key = $value;
-                            }
-
-                            break;
+        foreach ($psr4Dirs as $prefix => $dir) {
+            \CaoJiayuan\Utility\file_map($dir, function ($path, \SplFileInfo $fileInfo, $isDir) use ($dir, $prefix, $afterEach) {
+                if (!$isDir) {
+                    $class = $prefix . substr(str_replace([$dir, '/'], ['', '\\'], $path), 0, -4);
+                    $props = self::getPropertyInfo()->getProperties($class);
+                    $cacheKey = md5($class);
+                    $path = self::getCacheDir($cacheKey);
+                    $data = [];
+                    foreach ($props as $prop) {
+                        $types = self::getPropertyInfo()->getTypes($class, $prop);
+                        if ($types) {
+                            $t = $types[0];
+                        } else {
+                            $t = self::makeDefaultType();
                         }
-                    } else {
-                        $instance->$key = $value;
+                        $data[$prop] = self::makeCacheData($class, $prop, $t);
                     }
+                    file_put_contents($path, json_encode($data));
+                    $afterEach && $afterEach($class);
                 }
-            }
+            });
         }
-
-        return $instance;
     }
 
     public static function getPropertyInfo()
@@ -88,6 +71,170 @@ class Reflector
             $accessExtractors,
             $propertyInitializableExtractors
         );
+    }
+
+    public static function getCacheDir($key = '')
+    {
+        if (!self::$cacheDir) {
+            self::$cacheDir = __DIR__ . '/../_cache';
+        }
+        if (!file_exists(self::$cacheDir)) {
+            mkdir(self::$cacheDir);
+        }
+
+        return self::$cacheDir . DIRECTORY_SEPARATOR . $key;
+    }
+
+    /**
+     * @param mixed $cacheDir
+     */
+    public static function setCacheDir($cacheDir)
+    {
+        self::$cacheDir = $cacheDir;
+    }
+
+    protected static function makeDefaultType()
+    {
+        return new Type(Type::BUILTIN_TYPE_STRING, true);
+    }
+
+    protected static function makeCacheData($class, $prop, Type $type)
+    {
+        $collectionKeyType = $type->getCollectionKeyType();
+        $collectionValueType = $type->getCollectionValueType();
+
+        return [
+            'builtinType'         => $type->getBuiltinType(),
+            'nullable'            => $type->isNullable(),
+            'class'               => $type->getClassName(),
+            'collection'          => $type->isCollection(),
+            'collectionKeyType'   => $collectionKeyType ? self::makeCacheData($class, $prop, $collectionKeyType) : null,
+            'collectionValueType' => $collectionValueType ? self::makeCacheData($class, $prop, $collectionValueType) : null,
+        ];
+    }
+
+    public static function clearCaches()
+    {
+        @unlink(self::getCacheDir());
+    }
+
+    public static function loadFromArray($clz, $data)
+    {
+        if (is_object($clz)) {
+            $instance = $clz;
+        } else {
+            $instance = new $clz;
+        }
+
+        foreach ($data as $key => $value) {
+            $setMethod = 'set' . ucfirst($key);
+            if (method_exists($instance, $setMethod)) {
+                $instance->$setMethod($value);
+            } else if (property_exists($instance, $key)) {
+                if (is_null($value)) {
+                    $instance->$key = null;
+                } else {
+                    $type = self::getTypeFromCache($clz, $key);
+                    if ($type) {
+                        $builtinType = $type->getBuiltinType();
+                        $class = $type->getClassName();
+                        $collection = $type->isCollection();
+                        if ($builtinType == 'object' && is_subclass_of($class, ModelReflector::class)) {
+                            self::validateReflectData($value, $class);
+                            $instance->$key = self::loadFromArray($class, $value);
+                        } elseif (self::isCommonType($builtinType)) {
+                            $instance->$key = self::castCommonType($builtinType, $value);
+                        } elseif ($collection) {
+                            $ct = $type->getCollectionValueType();
+                            $itemClass = $ct->getClassName();
+                            if (!is_subclass_of($itemClass, ModelReflector::class)) {
+                                throw new \InvalidArgumentException("Model property [{$key}] must an array of instance of ModelReflector");
+                            }
+                            foreach ($value as $k => $v) {
+                                self::validateReflectData($value, $itemClass);
+                                $instance->$key[$k] = self::loadFromArray($itemClass, $v);
+                            }
+                        } else {
+                            $instance->$key = $value;
+                        }
+                    } else {
+                        $instance->$key = $value;
+                    }
+                }
+            }
+        }
+
+        return $instance;
+    }
+
+    protected static function getTypeFromCache($class, $prop)
+    {
+        if ($type = self::getCacheType($class, $prop)) {
+            return $type;
+        }
+
+        $types = self::getPropertyInfo()->getTypes($class, $prop);
+
+        if ($types) {
+            $t = $types[0];
+        } else {
+            $t = self::makeDefaultType();
+        }
+
+        return self::putCacheType($class, $prop, $t);
+    }
+
+    protected static function getCacheType($class, $prop)
+    {
+        $data = null;
+        if (array_key_exists($class, self::$cacheClasses)) {
+            $data = self::$cacheClasses[$class];
+        } else {
+            $cacheKey = md5($class);
+            $path = self::getCacheDir($cacheKey);
+            if (file_exists($path)) {
+                $data = json_decode(file_get_contents($path), true);
+                $data && self::$cacheClasses[$class] = $data;
+            }
+        }
+
+        if ($data && array_key_exists($prop, $data)) {
+            return self::makeTypeFromCacheData($data[$prop]);
+        }
+
+        return null;
+    }
+
+    protected static function makeTypeFromCacheData($data)
+    {
+        if (is_null($data)) {
+            return null;
+        }
+
+        return new Type($data['builtinType'], $data['nullable'], $data['class'],
+            $data['collection'], self::makeTypeFromCacheData($data['collectionKeyType']), self::makeTypeFromCacheData($data['collectionValueType']));
+    }
+
+    protected static function putCacheType($class, $prop, Type $type)
+    {
+        $data = [];
+        $cacheKey = md5($class);
+        $path = self::getCacheDir($cacheKey);
+
+        if (array_key_exists($class, self::$cacheClasses)) {
+            $data = self::$cacheClasses[$class];
+        } else {
+            if (file_exists($path)) {
+                $data = json_decode(file_get_contents($path), true);
+                $data && self::$cacheClasses[$class] = $data;
+            }
+        }
+
+        $data[$prop] = self::makeCacheData($class, $prop, $type);
+
+        file_put_contents($path, json_encode($data));
+
+        return $type;
     }
 
     protected static function validateReflectData($value, $class)
@@ -138,71 +285,6 @@ class Reflector
             default:
                 return $value;
         }
-    }
-
-    public static function getCacheDir($key = '')
-    {
-        if (!self::$cacheDir) {
-            self::$cacheDir =  __DIR__ . '/../_cache';
-        }
-        if (!file_exists(self::$cacheDir)) {
-            mkdir(self::$cacheDir);
-        }
-
-        return self::$cacheDir . DIRECTORY_SEPARATOR . $key;
-    }
-
-    /**
-     * @param mixed $cacheDir
-     */
-    public static function setCacheDir($cacheDir)
-    {
-        self::$cacheDir = $cacheDir;
-    }
-
-    protected static function getTypeFromCache($class, $prop)
-    {
-        $types = self::getPropertyInfo()->getTypes($class, $prop);
-        $cacheKey = md5($class);
-
-        if ($types) {
-
-            $cacheData = self::makeCacheData($class, $prop, $types[0]);
-        }
-
-
-        return null;
-    }
-
-    protected static function getCacheData($class, $prop)
-    {
-        $cacheKey = md5($class);
-
-        $path = self::getCacheDir($cacheKey);
-        if (file_exists($path)) {
-            $data = json_decode(file_get_contents($path), true);
-            if ($data && array_key_exists($prop, $data)) {
-
-            }
-
-        }
-
-        return null;
-    }
-
-    protected static function makeCacheData($class, $prop, Type $type)
-    {
-        $collectionKeyType = $type->getCollectionKeyType();
-        $collectionValueType = $type->getCollectionValueType();
-
-        return [
-            'builtinType'         => $type->getBuiltinType(),
-            'nullable'            => $type->isNullable(),
-            'class'               => $type->getClassName(),
-            'collection'          => $type->isCollection(),
-            'collectionKeyType'   => $collectionKeyType ? self::makeCacheData($class, $prop, $collectionKeyType) : null,
-            'collectionValueType' => $collectionValueType ? self::makeCacheData($class, $prop, $collectionValueType) : null,
-        ];
     }
 
 }
